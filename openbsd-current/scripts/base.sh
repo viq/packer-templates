335e3fe3957f1ab50db507ae720e1f843891b27c
viq <vicviq@gmail.com>
2014-04-14 13:17:55 +0200

    Update mirror to a faster one

--- openbsd-current/scripts/base.sh
+++ openbsd-current/scripts/base.sh
@@ -1,7 +1,7 @@
 # set system pkg path
 echo " "
 echo "Setting system PKG_PATH"
-echo "installpath = http://ftp.eu.openbsd.org/pub/OpenBSD/snapshots/packages/$(arch -s)" > /etc/pkg.conf
+echo "installpath = http://ftp2.eu.openbsd.org/pub/OpenBSD/snapshots/packages/$(arch -s)" > /etc/pkg.conf
 
 # install wget/curl/bash/vim and its dependencies
 echo " "
