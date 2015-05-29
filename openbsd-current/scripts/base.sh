bdd4c77c1a19f0c1d3aa33f3b911d8cafd33b7b5
viq <vicviq@gmail.com>
2015-05-29 22:18:44 +0200

    Adapt to changes in install process

--- openbsd-current/scripts/base.sh
+++ openbsd-current/scripts/base.sh
@@ -1,13 +1,14 @@
 # set system pkg path
 echo " "
 echo "Setting system PKG_PATH"
-echo "installpath = http://ftp5.eu.openbsd.org/ftp/pub/OpenBSD/snapshots/packages/$(arch -s)" > /etc/pkg.conf
+sudo sh -c 'echo "installpath = http://ftp5.eu.openbsd.org/ftp/pub/OpenBSD/snapshots/packages/$(arch -s)" > /etc/pkg.conf'
+#echo "installpath = http://ftp2.eu.openbsd.org/pub/OpenBSD/snapshots/packages/$(arch -s)" > /etc/pkg.conf
 
 # install wget/curl/bash/vim and its dependencies
 echo " "
 echo " Installing needed packages "
 echo " "
-pkg_add wget curl bash vim--no_x11 rsync-- bzip2 ngrep cvsutils git
+sudo pkg_add wget curl bash vim--no_x11 rsync-- bzip2 ngrep cvsutils git
 
 # giving root & vagrant bash as shell
 #echo " "
@@ -19,11 +20,11 @@ pkg_add wget curl bash vim--no_x11 rsync-- bzip2 ngrep cvsutils git
 # sudo
 # Defaults requiretty is not present in the sudoers file
 # env_keep I'll leave it as it is since user's path is same or more comprehensive than root's path
-echo " "
-echo " Setting sudo to work with vagrant "
-echo " "
-echo "# Uncomment to allow people in group wheel to run all commands without a password" >> /etc/sudoers
-echo "%wheel        ALL=(ALL) NOPASSWD: SETENV: ALL" >> /etc/sudoers
+#echo " "
+#echo " Setting sudo to work with vagrant "
+#echo " "
+#echo "# Uncomment to allow people in group wheel to run all commands without a password" >> /etc/sudoers
+#sudo sh -c 'echo "%wheel        ALL=(ALL) NOPASSWD: SETENV: ALL" >> /etc/sudoers'
 
-echo "SUDO=sudo -E" >> /etc/mk.conf
-echo 'PORTSDIR_PATH=${PORTSDIR}:$(PORTSDIR)/openbsd-wip:${PORTSDIR}/mystuff' >> /etc/mk.conf
+sudo sh -c 'echo "SUDO=sudo -E" >> /etc/mk.conf'
+sudo sh -c "echo 'PORTSDIR_PATH=${PORTSDIR}:$(PORTSDIR)/openbsd-wip:${PORTSDIR}/mystuff' >> /etc/mk.conf"
--- openbsd-current/scripts/vagrant.sh
+++ openbsd-current/scripts/vagrant.sh
@@ -1,27 +1,27 @@
 # Set up Vagrant.
 
-date > /etc/vagrant_box_build_time
+sudo sh -c "date > /etc/vagrant_box_build_time"
 
 # Create puppet user/group
 echo " "
 echo " Creating puppet user / group "
 echo " "
-groupadd puppet
-useradd -g puppet -d /var/lib/puppet -s /usr/bin/false puppet
+sudo groupadd puppet
+sudo useradd -g puppet -d /var/lib/puppet -s /usr/bin/false puppet
 
 # Add groups puppet and chef
-groupadd puppet
-groupadd chef
+sudo groupadd puppet
+sudo groupadd chef
 
 # setup the vagrant key
 # you can replace this key-pair with your own generated ssh key-pair
 echo " "
 echo " Setting the vagrant ssh pub key "
 echo " "
-mkdir /home/vagrant/.ssh
-chmod 700 /home/vagrant/.ssh
-chown vagrant.vagrant /home/vagrant/.ssh
-touch /home/vagrant/.ssh/authorized_keys
-ftp -o /home/vagrant/.ssh/authorized_keys https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub
-chmod 600 /home/vagrant/.ssh/authorized_keys
+sudo -u vagrant mkdir /home/vagrant/.ssh
+sudo -u vagrant chmod 700 /home/vagrant/.ssh
+sudo -u vagrant chown vagrant.vagrant /home/vagrant/.ssh
+sudo -u vagrant touch /home/vagrant/.ssh/authorized_keys
+sudo -u vagrant ftp -o /home/vagrant/.ssh/authorized_keys https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub
+sudo -u vagrant chmod 600 /home/vagrant/.ssh/authorized_keys
 chown vagrant.vagrant /home/vagrant/.ssh/authorized_keys
