26addeaeec7b810ae311735b4a83321738cfae13
viq <vicviq@gmail.com>
2017-08-02 22:49:21 +0200

    Use ftp2.eu.openbsd.org and update to openbsd61

--- openbsd-current/update_template.sh
+++ openbsd-current/update_template.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-sed s/CKSUM64/`curl http://ftp2.eu.openbsd.org/pub/OpenBSD/snapshots/amd64/SHA256 2> /dev/null | grep install..\.iso | cut -d' ' -f4`/ template.json.tmpl | \
-sed s/CKSUM32/`curl http://ftp2.eu.openbsd.org/pub/OpenBSD/snapshots/i386/SHA256 2> /dev/null | grep install..\.iso | cut -d' ' -f4`/ > template.json
+sed s/CKSUM64/`curl https://ftp.eu.openbsd.org/pub/OpenBSD/snapshots/amd64/SHA256 2> /dev/null | grep install..\.iso | cut -d' ' -f4`/ template.json.tmpl | \
+sed s/CKSUM32/`curl https://ftp.eu.openbsd.org/pub/OpenBSD/snapshots/i386/SHA256 2> /dev/null | grep install..\.iso | cut -d' ' -f4`/ > template.json
 #sed s/CKSUM64/`curl http://ftp5.eu.openbsd.org/ftp/pub/OpenBSD/snapshots/amd64/SHA256 2> /dev/null | grep install..\.iso | cut -d' ' -f4`/ template.json.tmpl | \
 #sed s/CKSUM32/`curl http://ftp5.eu.openbsd.org/ftp/pub/OpenBSD/snapshots/i386/SHA256 2> /dev/null | grep install..\.iso | cut -d' ' -f4`/ > template.json
-if [ ! -f http/site60.tgz ] ; then
-    cd site && tar zcf ../http/site60.tgz .
+if [ ! -f http/site61.tgz ] ; then
+    cd site && tar zcf ../http/site61.tgz .
 fi
